61532f4970a6ea21bb490155c76d4c5380737c3c
anisbhati <anisbhati101@gmail.com>
2022-12-20 13:40:28 +0530

    project inintiate

---
+++ src/main/java/com/example/demo/JwtDemo11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtDemo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemo11Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/democonfig/MySecurityConfig.java
@@ -0,0 +1,5 @@
+package com.example.democonfig;
+
+public class MySecurityConfig {
+
+}
